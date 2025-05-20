f4a839e8a9e041431a8a841ed504c06191b0f29c
jemmybutton <cph.lmy@gmail.com>
2025-05-21 00:14:38 +0300

    one more glyph, more sources, multiple affixes can now be added, improved spacing

--- processfont.py
+++ processfont.py
@@ -1,10 +1,13 @@
 import fontforge, psMat
 font = fontforge.open("tenevil-font.svg")
+font.encoding = "UnicodeFull"
 glyphs = font.selection.all()
 glyphmove = psMat.translate(0,-300)
-font.transform(glyphmove) 
+font.transform(glyphmove)
 font.autoWidth(150)
 font.addLookup('gen_kern', 'gpos_pair', (), [['liga', [['latn', ['dflt']]]]])
 font.addLookupSubtable('gen_kern','gen_kern_subt')
 font.autoKern('gen_kern_subt',150,touch=1)
+glyphs = font.selection.select(("ranges",None),0xf0080,0xf00ff)
+font.autoWidth(50)
 font.generate("tenevil-font.otf")
